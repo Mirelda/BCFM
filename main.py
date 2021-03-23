1c0bd6a8fc35856a2d669cac35c6335126611245
Mirelda <58131945+Mirelda@users.noreply.github.com>
2021-03-23 21:24:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Mirelda Diker<h1>"
+
+@app.route('/whoami', methods = ['GET'])
+def whoami():
+    x = request.args
+    return jsonify({'firstname': x.get('firstname',''),
+            'lastname':x.get('lastname','')})
+
+
+
+    
+app.run(debug=True)
